a44818f6ef26b775b0bd8d9fa75c4c0d8dbfa87a
Lê Bá Dũng <lebadung178@gmail.com>
2024-03-19 00:04:15 +0700

    Cài đặt CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const database = require("./config/database");
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const routesApiVer1 = require("./api/v1/routes/index.route");
 
@@ -9,6 +10,15 @@ database.connect();
 
 const app = express();
 const port = process.env.PORT;
+
+// var corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
