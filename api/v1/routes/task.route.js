80a780d96cc48427bcdd1c5568119c536c3672a5
Lê Bá Dũng <lebadung178@gmail.com>
2024-03-18 23:26:59 +0700

    Thêm mới công việc

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/task.model");
-const controllers = require("../controllers/task.controller");
+const controller = require("../controllers/task.controller");
 
-router.get("/", controllers.index);
+router.get("/", controller.index);
 
-router.get("/detail/:id", controllers.detail);
+router.get("/detail/:id", controller.detail);
 
-router.patch("/change-status/:id", controllers.changeStatus);
+router.patch("/change-status/:id", controller.changeStatus);
 module.exports = router;
 
-router.patch("/change-multi/", controllers.changeMulti);
+router.patch("/change-multi/", controller.changeMulti);
+
+router.post("/create", controller.create);
 module.exports = router;
