4a97ecd09be0f135390209b040fe2c09feeb98bb
Lê Bá Dũng <lebadung178@gmail.com>
2024-03-18 23:28:55 +0700

    Chỉnh sửa công việc

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -13,4 +13,6 @@ module.exports = router;
 router.patch("/change-multi/", controller.changeMulti);
 
 router.post("/create", controller.create);
+
+router.patch("/edit/:id", controller.edit);
 module.exports = router;
