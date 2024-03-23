6efc4b36ad492eef7c1956f78215d6b5e457bd1d
Lê Bá Dũng <lebadung178@gmail.com>
2024-03-23 19:44:33 +0700

    Tính năng đăng ký

---
+++ api/v1/controllers/user.controller.js
@@ -0,0 +1,31 @@
+const md5 = require("md5");
+const User = require("../models/user.model");
+
+const generateHelper = require("../../../helpers/generate.helper");
+
+// [POST] /api/v1/users/register
+module.exports.register = async (req, res) => {
+  const emailExist = await User.findOne({
+    email: req.body.email,
+    deleted: false
+  });
+
+  if(emailExist) {
+    res.json({
+      code: 400,
+      message: "Email đã tồn tại!"
+    });
+  } else {
+    req.body.password = md5(req.body.password);
+    req.body.token = generateHelper.generateRandomString(30);
+
+    const user = new User(req.body);
+    const data = await user.save();
+
+    res.json({
+      code: 200,
+      message: "Tạo tài khoản thành công!",
+      token: data.token
+    });
+  }
+};
